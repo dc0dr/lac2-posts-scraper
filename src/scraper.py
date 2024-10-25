720c812d7c7fcaa1876bf4560bb054dad51ec4cb
dc0dr <ecdaniels826@outlook.com>
2024-10-25 13:27:23 +0000

    feat: added a URN extraction to get post link

--- src/scraper.py
+++ src/scraper.py
@@ -57,3 +57,42 @@ class PostScraper:
         else:
             print("Failed to retrieve post content:", f"{response.json().get('message')}.", "Status code:", response.status_code)
             return None
+
+    def extract_post_links(post_links):
+        posts = post_links.get('elements', [])
+
+        extracted_links = []
+
+        for post in posts:
+            post_url = f"https://www.linkedin.com/feed/update/{post['id']}"
+            extracted_links.append({
+                'url': post_url
+            })
+            
+        return extracted_links
+
+    def extract_post_id(post_links):
+        post_ids = post_links.get('elements', [])
+        extracted_post_ids = []
+
+        for post in post_ids:
+            post_id = post['id']
+            extracted_post_ids.append({
+                'id': post_id
+            })
+
+        return extracted_post_ids
+
+    post_links = get_post_links(ACCESS_TOKEN, ORGANIZATION_ID)
+
+    
+    if post_links:
+        extracted_links = extract_post_links(post_links)
+        extracted_post_ids = extract_post_id(post_links)
+
+    for post, id in zip(extracted_links, extracted_post_ids):
+        post_content = get_post_content(ACCESS_TOKEN, id['id'])
+        post_id = id['id']
+        commentary = post_content['results'][post_id]['commentary']
+
+        print(f'Post URL: {post["url"]}')
