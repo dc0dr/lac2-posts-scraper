11d758124f595db9a17b26901f3745efef96bb5d
dc0dr <ecdaniels826@outlook.com>
2024-11-20 10:16:21 +0000

    model: changed prompt to provide shorter descriptions

--- src/post_descriptor.py
+++ src/post_descriptor.py
@@ -16,7 +16,13 @@ class Summarizer:
         summary = ollama.chat(model='llama3.2', messages=[
             {
                 'role': 'user',
-                'content': f'Summarize this post concisely and result you give me must be nothing but the summarized post: {post_content}'
+                'content': f'''
+                Take this text: {post_content}.
+                
+                Summarize it in less than fifteen words to focus on the key topics and omit unnecessary details. 
+                If the main text is not in English, translate it to English first before summarizing it.
+                The result should only be the text you summarized. Do not add anything to it.
+                ''',
             }
         ])
         
